c4c25c3f239e9f04c78048a4d820c63cc60b48d9
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2022-06-25 08:25:31 -0600

    jq is already installed

--- run.sh
+++ run.sh
@@ -12,6 +12,4 @@ yarn typedoc
 # You can add additional commands here to make assertions on the output,
 # here's one example checking the parsed name from package.json
 
-curl https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 --output jq
-chmod +x jq
-test $(jq '.name' docs/out.json) = "typedoc-reprosx"
+test $(jq '.name' docs/docs.json) = "typedoc-reprosx"
