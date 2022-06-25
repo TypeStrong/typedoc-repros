5601c69bd7bb028c16e0efeca0ccc3674891f320
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2022-06-25 08:23:22 -0600

    Example of using jq

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+# Automatically exit with an error code if any command fails
+set -e
+
 # Install package.json dependencies
 yarn
 
 # Run TypeDoc
 yarn typedoc
 
-echo "Process exited with code: $?"
-echo
-echo
+# You can add additional commands here to make assertions on the output,
+# here's one example checking the parsed name from package.json
+
+curl https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 --output jq
+chmod +x jq
+test $(jq '.name' docs/out.json) = "typedoc-reprosx"
