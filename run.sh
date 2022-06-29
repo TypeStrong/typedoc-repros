d6ab77eac4d6a2f37151f21e278b4e9e03394374
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2022-06-28 20:46:12 -0600

    Set up repro for TypeStrong/typedoc#1969

--- run.sh
+++ run.sh
@@ -20,4 +20,6 @@ set -v
 # if TypeDoc's output doesn't match what you expected. Here's one example
 # checking that the name from package.json is used in TypeDoc's output.
 
-test $(jq '.name' docs/docs.json) = '"typedoc-repros"'
+# test $(jq '.name' docs/docs.json) = '"typedoc-repros"'
+
+grep docs/docs.json "A comment for a method"
