c7dbfe44b3fc689d63d35c5581816ff228ff0a24
CH <clint@thestaticvoid.net>
2022-06-30 11:24:26 +0200

    module comments bug?

---
+++ src/fns.ts
@@ -0,0 +1,29 @@
+/**
+ * @module
+ * Module comment for fns.ts in the file?
+ */
+
+/**
+ * Comment for a
+ * @param p Some parameter
+ */
+export const a = (p:string) => {
+
+};
+
+/**
+ * Comment for b
+ * 
+ * @remarks
+ * Remarks for b
+ */
+export const b = () => {
+
+}
+
+/**
+ * Comment for c
+ */
+export const c = () => {
+
+}
---
+++ src/fnsAlt.ts
@@ -0,0 +1,25 @@
+/**
+ * @module
+ * Module comment for fnsAlt.ts in the file?
+ */
+
+/**
+ * Comment for aa
+ */
+export const aa = () => {
+
+};
+
+/**
+ * Comment for bb
+ */
+export const bb = () => {
+
+}
+
+/**
+ * Comment for cc
+ */
+export const cc = () => {
+
+}
