6397d1340044c028c4dc8ec4b9d668b6b8f27c42
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2022-07-05 19:15:33 -0600

    Add second param

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export class ContactManagement {
 	 * Constructor for the Contact management object.
 	 *
 	 * @param {contactData.dataType} [data={}] - The given contact values <--- line 95
+	 * @param {contactData.dataType} [data2={}] - The given contact values <--- line 95
 	 */
-	public constructor(data: any = {}) {}
+	public constructor(data = {}, data2 = {}) {}
 }
