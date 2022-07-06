d844ca498a9803b0e0cc2e3633556195138e321b
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2022-07-05 19:14:18 -0600

    Add repro code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-/**
- * Some code reproducing a bug.
- */
-export const bug = 123;
+export class ContactManagement {
+	/**
+	 * Constructor for the Contact management object.
+	 *
+	 * @param {contactData.dataType} [data={}] - The given contact values <--- line 95
+	 */
+	public constructor(data: any = {}) {}
+}
