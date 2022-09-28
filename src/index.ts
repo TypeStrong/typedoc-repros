73f0600a1d86c60e1335b6b869926def938f6024
Robert Huitl <git@huitl.de>
2022-09-28 15:46:12 +0200

    Private constructor assignment properties are not extracted.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,38 @@
 /**
- * Some code reproducing a bug.
+ * A class to be documented.
  */
-export const bug = 123;
+export class Test
+{
+	/**
+	 * A private field.
+	 */
+	private privateField: string = "";
+
+	/**
+	 * A protected field.
+	 */
+	protected protectedField: string = "";
+
+	/**
+	 * A public field.
+	 */
+	public publicField: string = "";
+
+	constructor(
+		/**
+		 * A private field declared in a constructor assignment.
+		 */
+		private anotherPrivateField: string,
+
+		/**
+		 * A protected field declared in a constructor assignment.
+		 */
+		protected anotherProtectedField: string,
+
+		/**
+		 * A public field declared in a constructor assignment.
+		 */
+		public anotherPublicField: string
+	)
+	{ }
+}
