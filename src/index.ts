ce216148e92af971fa4b361e661903ae76f0a7ab
Jon Hardy <jonathan.c.hardy@gmail.com>
2023-08-25 10:38:11 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 /**
- * The signature for a function acting as an event handler.
+ * A class.
  */
-export type EventHandler = ((e: Event) => void);
+class MyClass {
+  /**
+   * My summary documentation, which is duplicated.
+   * @param digits - must be a non-negative integer
+   * @returns a function that converts a string to a string without so many decimals
+   * @see See string is also duplicated.
+   */
+  public static stringifyFixed(digits: number): (val: string) => string {
+    return (val: string) => parseFloat(val).toFixed(digits).toString();
+  }
+}
