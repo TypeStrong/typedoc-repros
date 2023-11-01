fa42e96ca75fc790732436ae62ff918b14d47e02
Timothee Guerin <tiguerin@microsoft.com>
2023-11-01 10:57:11 -0700

    Repro for multiple declaration warning

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
-/**
- * Some code reproducing a bug.
- */
-export const bug = 123;
+export interface TemplatedTypeBase {
+  /**
+   * Doc here
+   */
+  prop?: string[];
+}
+
+export interface One extends TemplatedTypeBase {}
+
+export interface Two extends TemplatedTypeBase {}
+
+export type Type = One | Two;
+
+// errors
+export function isTemplateInstance(
+  type: Type
+): type is Type & { prop: string[] } {
+  return true;
+}
+
+// also error
+export function isTemplateInstance2(
+  type: Type
+): type is Type & Required<TemplatedTypeBase> {
+  return true;
+}
+
+// this works
+export function isTemplateInstance3(
+  type: Type
+): type is Type & TemplatedTypeBase {
+  return true;
+}
