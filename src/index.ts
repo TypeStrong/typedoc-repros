9af9babb472d933e1516e45aa5fcc942459c21d0
Lawrence Forooghian <lawrence@forooghian.com>
2024-01-22 14:28:43 +0000

    Demonstrate bug in link text when using a meaning

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 /**
- * Some code reproducing a bug.
+ * A thing that you should know about `foo` is that it’s called `foo`.
  */
-export const bug = 123;
+export const foo = 123;
+
+/**
+ * Here I’m writing about `bar` but I really want to tell you about {@link foo:var}.
+ */
+export const bar = 456;
