8c75e36f61a3912d9dd0170065c7e1e1127fae3f
Steven Luscher <steveluscher@users.noreply.github.com>
2025-01-03 20:15:58 -0500

    …

--- src/something.ts
+++ src/something.ts
@@ -1,9 +1,14 @@
+/** @interface */
 export type Something = {
     /**
+     * Some function
+     * 
+     * @overload Default
      * {@label DEFAULT}
      */
     someFunction(): void;
     /**
+     * @overload With number
      * {@label WITHNUMBER}
      */
     someFunction(num: number): void;
